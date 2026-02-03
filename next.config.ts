f99c196d1c79f3c6864d1849b4735ff4fccec30b
tddv2017 <tddv2017@gmail.com>
2026-02-03 21:33:36 +0700

    commit1

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 👇 CHỈ GIỮ LẠI ĐÚNG CÁI NÀY
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Tuyệt đối KHÔNG có dòng 'eslint' ở đây
 };
 
 export default nextConfig;
